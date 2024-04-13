6f544d1b5e676867dffbd52354be445887eb99ff
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-13 15:05:17 +0530

    Practical 2[b]

---
+++ Practical 2/[2b].Separating-Vowels-and-Consonants-in-String.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class lab2b{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        int vow = 0, con = 0, dig = 0, sp = 0;
+
+        for(int i=0; i<input.length(); i++){
+            char x = input.charAt(i);
+            if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+                vow++;
+            }
+            else if (Character.isLetter(x)){
+                con++;
+            } 
+            else if (Character.isDigit(x)){
+                dig++;
+            } 
+            else{
+                sp++;
+            }
+        }
+
+        System.out.println("Vowels are: " + vow);
+        System.out.println("Consonents are: " + con);
+        System.out.println("Digits are: " + dig);
+        System.out.println("Special Characters are: " + sp);
+
+        System.out.println();
+        System.out.println("Developed by Arya");
+        sc.close();
+    } 
+}
