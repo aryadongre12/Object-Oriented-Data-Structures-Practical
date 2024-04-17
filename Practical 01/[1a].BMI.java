3556f5ca0442933c92e8925e108e11abfd6720c2
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-17 10:43:45 +0530

    Practical 1

---
+++ Practical 01/[1a].BMI.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class lab1a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter weight: ");
+        float w = sc.nextFloat();
+
+        System.out.println("Enter height: ");
+        float h = sc.nextFloat();
+
+        float bmi = w/(h*h);
+        System.out.println("Body Mass Index = " + bmi);
+        sc.close();
+    }
+}
